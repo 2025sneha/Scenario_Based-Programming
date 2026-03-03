502caa21d622a86d8e9c07afce6ff62c8b6a093b
Sneha Mothabhau Mohane <sneha.mohane1604@gmail.com>
2026-03-03 18:56:00 +0530

    Add files via upload

---
+++ program803.java
@@ -0,0 +1,28 @@
+ import java.util.Scanner;
+// fibonacci series 
+// 0 1 1 2 3 5 8 13 21.....
+
+class program803
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the number of elements in series : ");
+        int iSize = sobj.nextInt();
+
+        int i = 0;
+        long First = 0, Next = 0, Second = 0;
+
+        for(i = 1, First = 0, Second = 1; i <= iSize; i++)
+        {
+            System.out.println(First+ " ");
+            Next = First + Second;
+
+            First = Second;
+            Second = Next;
+        }
+        System.out.println();
+        sobj.close();
+    }
+}
